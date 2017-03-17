481f538b5f532d38da80707cd3cfd7927f49d161
KenenCS <KenenCS@163.com>
2017-03-17 17:07:57 +0800

    Initial Commit

---
+++ Pulse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pulse
+//
+//  Created by kenen on 2017/3/17.
+//  Copyright © 2017年 kenen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
